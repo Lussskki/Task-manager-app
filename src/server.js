e8a77722a3088d240bac3fb74754ee2f235b1777
Luka Guledani <lukaguledani123@gmail.com>
2024-11-14 23:58:41 +0400

    FEATURE: add connection, schema, body-parser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+import './db/connectMongo.js'
+import './db/schema/schema.js'
 import express from 'express'
 import signale from 'signale'
 import router from './router/firstRoute.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use('/', router)
 
 app.listen('3000', () =>{
