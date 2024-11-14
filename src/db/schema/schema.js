fabceea5c7eccde67aa6991f7eb4e1f638584840
Luka Guledani <lukaguledani123@gmail.com>
2024-11-15 00:03:00 +0400

    SCHEMA: for text to save in database

---
+++ src/db/schema/schema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const textSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    }
+    
+})
+
+const Text = mongoose.model('Text', textSchema)
+
+export default Text
