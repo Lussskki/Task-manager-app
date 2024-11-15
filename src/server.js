40655376fc81d87abc56aa519c04f4f9c8f63ecd
Luka Guledani <lukaguledani123@gmail.com>
2024-11-16 00:09:38 +0400

    FEATURE: added comments and secondRouter

--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ import './db/connectMongo.js'
 import './db/schema/schema.js'
 import express from 'express'
 import signale from 'signale'
-import router from './router/firstRoute.js'
+import router from './router/firstRouter.js'
+import secondRouter from './router/secondRouter.js'
 import bodyParser from 'body-parser'
 
 const app = express()
 
+// Use JSON parsing middleware
 app.use(bodyParser.json())
-app.use('/', router)
 
-app.listen('3000', () =>{
+// Use the routes
+app.use('/', router)
+app.use('/', secondRouter)
+
+app.listen('3000', () => {
     signale.success('Server running on port 3000')
 })
