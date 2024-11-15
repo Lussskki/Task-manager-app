395ab11c98d20d7d1530f67b968d5a9d574c3f1d
Luka Guledani <lukaguledani123@gmail.com>
2024-11-16 00:15:20 +0400

    ADDED: all necessary code to mongodb schema

--- src/db/schema/schema.js
+++ src/db/schema/schema.js
@@ -1,13 +1,33 @@
 import mongoose from "mongoose"
 
-const textSchema = new mongoose.Schema({
-    text: {
+// User schema for mongodb 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        validate: {
+            validator: function (v) {
+                // regex to check if contains "@"
+                 return /^.+@.+\..+$/.test(v)
+            },
+            message: props => `${props.value} is not valid email! Email must contain "@"`
+        }
+    },
+    password: {
         type: String,
         required: true
     }
-    
 })
 
-const Text = mongoose.model('Text', textSchema)
+const UserSchema= mongoose.model('Text', userSchema)
 
-export default Text
+export default UserSchema
