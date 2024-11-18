bfe49f614256a457d62d46d2530128d085013177
Luka Guledani <lukaguledani123@gmail.com>
2024-11-19 00:04:38 +0400

    SERVER: add protected route

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import signale from 'signale'
 import firstrouter from './router/firstRouter.js'
 import secondRouter from './router/secondRouter.js'
 import thirdRouter from './router/thirdRouter.js'
+import protectedRouter from './router/protectedRouter.js'
 
 import bodyParser from 'body-parser'
 
@@ -20,6 +21,7 @@ app.use(bodyParser.json())
 app.use('/', firstrouter)
 app.use('/', secondRouter)
 app.use('/', thirdRouter)
+app.use('/', protectedRouter)
 
 app.listen('3000', () => {
     signale.success('Server running on port 3000')
