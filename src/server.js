e9787b049ea85af6f4e6c746375ee1ef432b97c8
Luka Guledani <lukaguledani123@gmail.com>
2024-11-18 19:32:59 +0400

    REFACTOR: code base

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import './db/connectMongo.js'
 import './db/schema/schema.js'
+
 import express from 'express'
 import signale from 'signale'
-import router from './router/firstRouter.js'
+
+
+import firstrouter from './router/firstRouter.js'
 import secondRouter from './router/secondRouter.js'
+import thirdRouter from './router/thirdRouter.js'
+
 import bodyParser from 'body-parser'
 
 const app = express()
@@ -12,8 +17,9 @@ const app = express()
 app.use(bodyParser.json())
 
 // Use the routes
-app.use('/', router)
+app.use('/', firstrouter)
 app.use('/', secondRouter)
+app.use('/', thirdRouter)
 
 app.listen('3000', () => {
     signale.success('Server running on port 3000')
