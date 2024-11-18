3a5aa36775e968ebe0f65b167de6d08b05d2003e
Luka Guledani <lukaguledani123@gmail.com>
2024-11-18 19:52:38 +0400

    REFACTORED: sign up route

--- src/router/firstRouter.js
+++ src/router/firstRouter.js
@@ -1,19 +1,19 @@
 import express from 'express'
-import UserSchema from '../db/schema/schema.js'
+import UserSchema from '../db/schema/userSchema.js'
 import bcrypt from 'bcrypt'
 
-const router = express.Router()
+const firstRouter = express.Router()
 
 const saltRounds = 10
 
-// Signup route
-router.post('/signup', async (req, res) => {
-    // Get necessary information from the request body
+// Signup router
+firstRouter.post('/signup', async (req, res) => { 
+    // Necessary information from the request body
     const { name, lastName, email,  password } = req.body
 
     // Check if fields are present
     if (!email|| !password || !name || !lastName) {
-        return res.status(400).json({ message: 'Both text and password are required' })
+        return res.status(400).json({ message: ' All info are required' })
     }
     // Password characters
     if (password.length < 8 || password.length >16) {
@@ -27,14 +27,13 @@ router.post('/signup', async (req, res) => {
         // Create a new instance of Text with both text and password
         const newEmail = new UserSchema({ email, name, lastName, password: hashedPassword})
         
-        // Save to MongoDB
+        // Save to MongoDB 
         await newEmail.save();
         
-        res.status(201).json({ message: 'Email added successfully', newEmail })
-        console.log('Req body: ', req.body)
+        return res.status(201).json({ message: 'Email added successfully', newEmail })
     } catch (error) {
-        res.status(500).json({ message: 'Error adding', error })
+        return res.status(500).json({ message: 'Error with adding', error })
     }
-});
+})
 
-export default router
+export default firstRouter
